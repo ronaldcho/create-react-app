67bc7a20dd2e0cdc756decd87815277c8feecc83
Ronald Cho <ronaldcho@gmail.com>
2019-05-23 19:51:47 -0700

    src/components/Navigation/index.js

--- react-firebase-authentication/src/components/App/index.js
+++ react-firebase-authentication/src/components/App/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+import Navigation from '../Navigation';
+
+const App = () => ( 
+  <Router>
+    <Navigation />
+  </Router>
 );
 
 export default App;
--- react-firebase-authentication/src/components/Navigation/index.js
+++ react-firebase-authentication/src/components/Navigation/index.js
@@ -5,43 +5,26 @@ import {
 
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => ( <
-  div >
-  <
-  ul >
-  <
-  li >
-  <
-  Link to = {
-    ROUTES.SIGN_IN
-  } > Sign In < /Link> <
-  /li> <
-  li >
-  <
-  Link to = {
-    ROUTES.LANDING
-  } > Landing < /Link> <
-  /li> <
-  li >
-  <
-  Link to = {
-    ROUTES.HOME
-  } > Home < /Link> <
-  /li> <
-  li >
-  <
-  Link to = {
-    ROUTES.ACCOUNT
-  } > Account < /Link> <
-  /li> <
-  li >
-  <
-  Link to = {
-    ROUTES.ADMIN
-  } > Admin < /Link> <
-  /li> <
-  /ul> <
-  /div>
+const Navigation = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    </ul>
+  </div>);
 );
 
 export default Navigation;
